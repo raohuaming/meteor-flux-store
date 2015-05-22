2ade39cea58a5549c5980bebe186be7b7ffd0296
Huaming Rao <huaming.rao@gmail.com>
2015-05-22 16:48:43 +0800

    update to 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'huaming:flux-store',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'This is a Simple Store for meteor inspired by Facebook Flux, by using EventEmitter',
   // URL to the Git repository containing the source code for this package.
